493ffe12d51649c3306301b70d803f5ad1216cfc
Niklas Rönnholm <nikke.ronnholm@gmail.com>
2021-04-13 12:12:04 +0300

    some changes to production.js

--- security/production.js
+++ security/production.js
@@ -1,10 +1,14 @@
-export default (app, port) => {
+const production = (app, port) => {
     app.enable('trust proxy');
     app.use((req, res, next) => {
-        if (req.secure) next();
-
-        res.redirect(`https://${req.headers.host}${req.url}`);
+        if (req.secure) {
+            next();
+        } else {
+            res.redirect(`https://${req.headers.host}${req.url}`);
+        }
     });
 
     app.listen(port);
 }
+
+export default production;
